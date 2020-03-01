ea1af1d8e3c1033a0a10ecd964919d20c479c06a
LR <lr177245@gmail.com>
2020-03-01 11:47:31 -0800

    rot13 for review

--- rot13.js
+++ rot13.js
@@ -1,17 +1,17 @@
-//ROT13
 function rotate(chr, chrStart) {
   var ROTATE_AMOUNT = 13;
   var ALPHABET_LEN = 26;
  
   chrStart = chrStart.charCodeAt(0);
-  chr = chr.charCodeAt(0);
   
+  chr = chr.charCodeAt(0);
   chr -= chrStart;
   chr += ROTATE_AMOUNT;
   chr %= ALPHABET_LEN;
   chr += chrStart;
+  chr = String.fromCharCode(chr);
   
-  return String.fromCharCode(chr);
+  return chr;
 }
 
 function rot13(str) {
@@ -19,16 +19,16 @@ function rot13(str) {
   var chr;
   var len = str.length;
   var rotated = '';
-  var upperStart = 'A';
-  var lowerStart = 'a';
+  var UPPER_START = 'A';
+  var LOWER_START = 'a';
 
   for (i = 0; i < len; i++) {
     chr = str[i];
     
     if (chr >= 'A' && chr <= 'Z') {
-      chr = rotate(chr, upperStart);
+      chr = rotate(chr, UPPER_START);
     } else if (chr >= 'a' && chr <= 'z') {
-      chr = rotate(chr, lowerStart);
+      chr = rotate(chr, LOWER_START);
     }
 
     rotated += chr;
